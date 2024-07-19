72e82292297ccff305fe1d9960af27e38cafa3a5
pjerzyna <pawel.jerzyna@interia.pl>
2024-07-19 14:06:02 +0200

    update

---
+++ main.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import google.generativeai as genai
+from streamlit_elements import elements, mui
+
+api_key = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+# Hero section
+col1, col2 = st.columns(2, gap='small', vertical_alignment='center')
+with col1:
+    st.image('images/pawel_-cropped.png')
+
+with col2:
+    st.title("Hello! :eyes:", anchor="home")
+    st.write("I'm Pawel Jerzyna, a student of Automatics and Robotics. I'm curious about the world and passionate about new technology. In addition to my academic pursuits, "
+             "I dedicate my time to expanding my horizons through various activities, from exploring the world of folk music to engaging in sports, all while "
+             "striving to grow personally and professionally.")
+    #st.subheader("_Cracow, Poland_")
+    
+    # Social media 
+    social = [
+        ["Email", "mailto:pawel.jerzyna@gmail.com", "https://img.icons8.com/ios-glyphs/30/ffffff/email.png"],
+        ["GitHub", "https://github.com/pjerzyna", "https://img.icons8.com/ios-glyphs/30/ffffff/github.png"],
+        ["Instagram", "https://www.instagram.com/pawsonaura/", "https://img.icons8.com/ios-glyphs/30/ffffff/instagram.png"]
+    ]
+
+    social_html = '<div style="display: flex; gap: 10px;">'
+    for name, url, img_url in social:
+        social_html += f'<a href="{url}" target="_blank" title="{name}"><img src="{img_url}" alt="{name}" style="width: 30px; height: 30px;"/></a>'
+    social_html += '</div>'
+
+    st.markdown(social_html, unsafe_allow_html=True)
+
+# to customize personal bot
+persona = """ You are just a bot, Pawel's AI bot. This is very important, so it would be repeated again: you are Pawel's AI bot! 
+You help people answer questions about yourself. Answer as if you are responding.
+Don't answer in second or third person. If you don't know they answer you simply say "That's a secret", but do not abuse this, for example
+if question is from general knowledge (for example: "Which footballer has the most golden balls?" - simply answear - Leo Messi, e.t.c), just - well, answer it. 
+Here is more info about Pawel:
+
+This Pawel is currently on his 3rd year of studying Automatics and Robotics on faculty of Electrical Engineering, Automatics, Computer Science and
+Biomedical Engineering of AGH University of Cracow. He speaks three languages. Polish is his native language, and he speaks English fluently, both in speech and writing. 
+Additionally, he knows Russian, which he can communicate in, although without using complicated grammatical structures. 
+His studies allow him to explore advanced issues in the field of robotics, automation systems and modern information technologies. 
+He is particularly interested in robot design and programming, as well as artificial intelligence and its applications in everyday life and industry.
+Apart from science, he has recently become a member of the AVADER scientific club, where he works on various projects using drones.
+In his free time, he likes reading books and practicing sports, especially football. Moreover, for less than a year he has also 
+been a member of a folk band in which he sings and dances, which is a nice break from studying.
+
+His favourite dish is probably pizza, probably because, he loves everything (but not bananas) and has a huge sweet tooth at any time of the day or night.
+His favourite football team is Manchester United, and he doesnt have a special one favourite player, he likes a lot of them, he really admires CR7.
+Top few (not all) players (the order doesn't matter): Robert LewandowsKi, David de Gea, Lamine Yamal, Andrea Pirlo, Dani Carvajal, Hugo Lloris, 
+Marco Reus, Arjen Robben, Paulo Dybala, Tim Cahill, Bruno Fernandes, Alejandro Garnacho, Kobbie Mainoo, Phil Foden, Guillermo Ochoa and so far and so forth....   
+He is passionate about his future career as an engineer, believing that his education and interests will allow 
+him to develop innovative technological solutions that will have a real impact on the future. Apart from that, he is willing to
+be a good person and develop in many different areas, such as business. 
+
+"""
+
+st.title(" ")
+
+# wylapywanie z mikrofonu mowy i przerabianie jej na tekst pisany???????????????????????????????????????????????????????????????????????????
+st.header('My personal AI Bot', divider='gray')
+
+with st.form(key='my_form'):
+    user_question = st.text_input("Ask me something!")
+    submit_button = st.form_submit_button(label='Hit me!')
+
+if submit_button:
+    with st.spinner("Processing your question..."):
+        prompt = persona + "Here is the question that the user asked: " + user_question
+        response = model.generate_content(prompt)
+        st.write(response.text)
+
+st.title(" ")
+
+
+st.header("My Skills", divider='gray')
+st.write("""I don't consider myself a pure genius, but my wholelife studies took me to a certain level, and I hope my hard work pays off. I'm continuously 
+        learning and improving, always eager to expand my knowledge and skills. Even when things don't go as planned, I don't give up easily. 
+        I believe that persistence and a positive attitude towards challenges are key to achieving success.""")
+
+skills = {
+    "English": 80,
+    "Programming": 70,
+    "Positive Attitude": 100,
+    "Math": 75,
+    "Physics": 65,
+    "Soft Skills": 80,
+}
+
+#  to są paski postępu
+with elements("skills"):
+    for skill, proficiency in skills.items():
+        card_background = (
+            "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,69,0,1) 50%, rgba(255,165,0,1) 100%)"
+            if proficiency == 100
+            else "#36454f"
+        )
+        
+        mui.Card(
+            mui.CardContent(
+                mui.Typography(skill, variant="h6"),
+                mui.LinearProgress(
+                    variant="determinate",
+                    value=proficiency,
+                    sx={
+                        "height": 10,
+                        "marginTop": 2,
+                    }
+                ),
+            ),
+            sx={
+                "marginBottom": 2,
+                "background": card_background,
+                "color": "white",
+                "padding": "10px", 
+            }
+        )
+
